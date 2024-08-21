279937ca1dd0eedc18bf52efff21bae6fccf7446
Igor Vinnícius <igorquintao22@gmail.com>
2024-08-20 22:19:52 -0300

    Adicionando a classe Processo de Venda que é responsável por processar uma venda aplicando juros

--- src/servico/ProcessoDeVenda.java
+++ src/servico/ProcessoDeVenda.java
@@ -1,5 +1,41 @@
 package servico;
 
+import java.time.LocalDate;
+
 public class ProcessoDeVenda {
+	
+	Pagamento pagamento;
+	
+	public ProcessoDeVenda() {
+	}
+
+	public ProcessoDeVenda(Pagamento pagamento) {
+		super();
+		this.pagamento = pagamento;
+	}
+	
+	public void processarContrato(ContratoDeVenda contrato, int meses) {
+		double valorDividido = contrato.getPrecoTotal() / meses;
+		
+		for(int i = 1; i <= meses; i++) {
+			LocalDate dataVencimento = contrato.getDataAtual().plusMonths(i);
+			double juros = pagamento.juros(valorDividido, i);
+			double valor = valorDividido + juros;
+			contrato.getParcelas().add(new ParcelaFilme(dataVencimento, valor));
+		}
+	}
+	
+	public void processarAluguel(ContratoDeVenda contrato, int dias) {
+		double valorAluguel = contrato.getPrecoTotal() * 0.05;
+		
+		for(int i = 1; i <= dias; i++) {
+			LocalDate dataVencimento = contrato.getDataAtual().plusDays(i);
+			contrato.getAluguel().add(new AluguelFilme(dataVencimento, valorAluguel));
+		}
+	}
+	
+	public double atrasoNaDevolucao(ContratoDeVenda contrato, long diasAtrasados) {
+		return contrato.getPrecoTotal() * Math.pow(1 + 0.1, diasAtrasados);
+	}
 
 }
